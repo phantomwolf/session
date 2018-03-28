370860e6824b2d45108933852909f637e1b09f3b
Nathan Zhao <jtzhao@suse.com>
2018-03-28 15:12:32 +0800

    update

--- session.go
+++ session.go
@@ -1,7 +1,9 @@
 package session
 
 import (
+	"crypto/rand"
 	"errors"
+	"math/big"
 	"time"
 
 	"github.com/satori/go.uuid"
@@ -9,8 +11,8 @@ import (
 )
 
 const (
-	uidKey    = "_uid"
-	expireKey = "_expire"
+	characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	length     = 32
 )
 
 var (
@@ -19,122 +21,41 @@ var (
 )
 
 type Session struct {
-	id     uuid.UUID
-	uid    string
-	expire time.Time
+	id     []byte
 	values map[string]string
 }
 
-// New returns a new Session which belongs to user <uid>
-// and expires after <dur>
-func New(uid string, dur time.Duration) (*Session, error) {
-	id, err := uuid.NewV4()
-	if err != nil {
-		log.Debugln("[session.go:New] UUID(version 4) generation failed")
-		return nil, err
+// New returns a new Session object
+func New() (*Session, error) {
+	max := big.NewInt(len(characters))
+	id := make([]byte, length)
+	for i := 0; i < length; i++ {
+		id[i] = characters[rand.Int(rand.Reader, max)]
 	}
-
 	sess := &Session{
 		id:     id,
-		uid:    uid,
-		expire: time.Now().Add(dur),
 		values: map[string]string{},
 	}
 	return sess, nil
 }
 
-// Load constructs an existing Session whose id is <key>
-// and other fields in <data>
-func Load(key string, data map[string]string) (*Session, error) {
-	id, err := uuid.FromString(key)
-	if err != nil {
-		log.Debugf("[session.go:Load] Invalid session id %s: %s\n", key, err.Error())
-		return nil, err
-	}
-
-	uid, ok := data[uidKey]
-	if !ok {
-		log.Debugf("[session.go:Load] User id not found in session %s\n", key)
-		return nil, ErrSessionMissingUID
-	}
-	delete(data, uidKey)
-
-	expire, err := time.Parse(time.RFC1123, data[expireKey])
-	if err != nil {
-		log.Debugf("[session.go:Load] Invalid date %s: %s\n", data[expireKey], err.Error())
-		return nil, err
-	}
-	delete(data, expireKey)
-
-	sess := &Session{
-		id:     id,
-		uid:    uid,
-		expire: expire,
-		values: data,
-	}
-	return sess, nil
-}
-
-// ToMap returns a map containing all the data of a Session except session id.
-// It's useful for storing the Session to storage.
-func (sess *Session) ToMap() map[string]interface{} {
-	data := map[string]interface{}{}
-	for k, v := range sess.values {
-		data[k] = v
-	}
-	data[expireKey] = sess.expire.Format(time.RFC1123)
-	data[uidKey] = sess.UID()
-	return data
-}
-
 // ID returns the string representation of session id(uuid)
 func (sess *Session) ID() string {
-	return sess.id.String()
+	return string(sess.id)
 }
 
-// UID returns the user id which the session belongs to
-func (sess *Session) UID() string {
-	return sess.uid
-}
-
-// SetUID sets the Session's uid to <uid>
-func (sess *Session) SetUID(uid string) {
-	sess.uid = uid
-}
-
-// GetVal retrieves the value associated with <key>
-func (sess *Session) GetVal(key string) (string, bool) {
+// Get retrieves the value associated with <key>
+func (sess *Session) Get(key string) (string, bool) {
 	val, ok := sess.values[key]
 	return val, ok
 }
 
-// SetVal sets the value associated with <key> to <val>
-func (sess *Session) SetVal(key string, val string) {
-	if key == expireKey || key == uidKey {
-		log.Panicf("[session.go:SetVal] Key can't be %s or %s\n", expireKey, uidKey)
-	}
+// Set sets the value associated with <key> to <val>
+func (sess *Session) Set(key string, val string) {
 	sess.values[key] = val
 }
 
 // DelVal deletes value associated with <key>
-func (sess *Session) DelVal(key string) {
+func (sess *Session) Del(key string) {
 	delete(sess.values, key)
-}
-
-// Expired returns true if Session is already expired; otherwise, return false.
-func (sess *Session) Expired() bool {
-	if sess.expire.Before(time.Now()) {
-		return true
-	}
-	return false
-}
-
-// SetExpire sets the expire time
-func (sess *Session) SetExpire(t time.Time) {
-	sess.expire = t
-}
-
-// SetExpireAfter sets the session to exipre after <d>
-func (sess *Session) SetExpireAfter(d time.Duration) {
-	sess.SetExpire(time.Now().Add(d))
 }
